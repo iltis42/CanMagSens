0969fd061819370c065b55cba910ef7dab9750bc
Helmut J. Rohs <helmut.j.rohs@web.de>
2025-02-03 22:50:36 +0200

    Clock and ticker with vector instead of set.

--- main/protocol/Clock.cpp
+++ main/protocol/Clock.cpp
@@ -12,18 +12,18 @@
 #include <freertos/FreeRTOS.h>
 #include <esp_attr.h>
 
-#include <set>
+#include <vector>
+#include <algorithm>
 
 esp_timer_handle_t Clock::_clock_timer = nullptr;
 
 static volatile unsigned long msec_counter = 0;
 
 // Simple fix number of slots registry
-// const int NRSLOTS = 4;
-static std::set<Clock_I*> clock_registry;
+static std::vector<Clock_I*> clock_registry;
 
 // Timer SR (called in a timer task context)
-static void IRAM_ATTR clock_timer_sr(std::set<Clock_I*> *registry)
+static void IRAM_ATTR clock_timer_sr(std::vector<Clock_I*> *registry)
 {
     // be in sync with millis, but sparse
     msec_counter = esp_timer_get_time() / 1000;
@@ -37,7 +37,10 @@ static void IRAM_ATTR clock_timer_sr(std::set<Clock_I*> *registry)
 // The clock
 Clock::Clock()
 {
-    // setup clock timer only once
+    clock_registry.resize(5);
+    clock_registry.clear();
+
+    // setup clock timer
     if ( _clock_timer == 0 ) {
         esp_timer_create_args_t t_args = {
             .callback = (esp_timer_cb_t)clock_timer_sr,
@@ -48,22 +51,30 @@ Clock::Clock()
 
         };
         esp_timer_create(&t_args, &_clock_timer);
-        esp_timer_start_periodic(_clock_timer, TICK_ATOM * 100); // the timers API is on usec, but buggy for the esp32c3 idf 4.3 (fixme)
-        clock_registry.clear();
+        esp_timer_start_periodic(_clock_timer, TICK_ATOM * 1000);
     }
 }
 
 
+static void remove_from_registry(Clock_I *cb)
+{
+    auto it = std::find(clock_registry.begin(), clock_registry.end(), cb);
+    if (it != clock_registry.end()) {
+        clock_registry.erase(it);
+    }
+}
 void Clock::start(Clock_I *cb)
 {
-    clock_registry.insert(cb);
+    esp_timer_stop(_clock_timer);
+    remove_from_registry(cb); // enforce one entry only
+    clock_registry.push_back(cb);
+    esp_timer_start_periodic(_clock_timer, TICK_ATOM * 1000);
 }
 void Clock::stop(Clock_I *cb)
 {
-    auto it = clock_registry.find(cb);
-    if ( it != clock_registry.end() ) {
-        clock_registry.erase(it);
-    }
+    esp_timer_stop(_clock_timer);
+    remove_from_registry(cb);
+    esp_timer_start_periodic(_clock_timer, TICK_ATOM * 1000);
 }
 
 unsigned long Clock::getMillis()
